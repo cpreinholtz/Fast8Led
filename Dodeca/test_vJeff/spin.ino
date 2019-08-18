3ae85093855f0337db94014beda952d79e0610f5
unknown <cpr978@gmail.com>
2019-08-18 02:20:43 -0700

    Rmove redundant function definition

--- Dodeca/test_vJeff/spin.ino
+++ Dodeca/test_vJeff/spin.ino
@@ -19,12 +19,7 @@ void spinArray() {
 }
 
 
-void testFixBlack(){
-  pixArray.setAtLeastActivePIX();//Random number of pixels
-  pixArray.spreadEvenly();
-  pixArray.setValue(CRGB::Black);
-  spinEvenly();
-}
+
 
 
 
